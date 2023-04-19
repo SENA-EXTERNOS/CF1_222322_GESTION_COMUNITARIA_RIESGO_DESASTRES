dd97e3590f8826dfc6593b5e884f6b552203d55d
Camilo Bolaño <80546125+elrilacho@users.noreply.github.com>
2023-04-19 11:45:19 -0500

    Update material

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -16,7 +16,7 @@
 
     figure.mb-5.mt-5
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/tfoCfQiwUlQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
         
 </template>
 
