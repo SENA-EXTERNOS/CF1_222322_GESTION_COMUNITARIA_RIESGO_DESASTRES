771b17e7c8c320f8d94ff6df6c60a982c1b2105d
Camilo Bolaño <80546125+elrilacho@users.noreply.github.com>
2023-04-19 11:34:09 -0500

    Componente revisado

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -1,12 +1,8 @@
 <template lang="pug">
 .curso-main-container.pb-3
-  BannerInterno
+  BannerInterno(icono="fas fa-tasks" titulo="Actividad didáctica")
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
-    .titulo-principal.color-acento-contenido
-      .titulo-principal__numero
-        span 
-          i.fas.fa-info
-      h1 Actividad didáctica   
+    
     .row.mb-5.justify-content-center.align-items-center.align-items-lg-stretch
       .col-6.col-md-4.col-lg-3.mb-4.mb-md-0
         .tarjeta.h-100.d-flex.align-items-center.p-4
